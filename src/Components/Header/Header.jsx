f89c30883337955f806a2e30fba04d4d9d912f4f
Dima Ivanov <latrey.9451@gmail.com>
2022-12-12 15:13:07 +0300

    header component links

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './Header.module.css'
 import headerLogo from '../../assets/main/header.svg'
 import button from '../../assets/SignIn/ButtonBack.png'
@@ -7,6 +7,8 @@ import photo from '../../assets/UserRecognition/userlogo.png'
 import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 
 export const Header = ({ user = 'User', userLogo = photo }) => {
+
+    const [dropMenu, setDropMenu] = useState(false)
     const locate = useLocation()
     const path = locate.pathname
     const navigate = useNavigate()
@@ -19,15 +21,68 @@ export const Header = ({ user = 'User', userLogo = photo }) => {
                     <NavLink to='/signin'><button className={s.button__in}>sign in</button></NavLink>
                 </div>)
             }
-            case  '/tablespage': {
+            case '/tablespage': {
                 return (
                     <div className={s.header_container}>
-                        <div className={s.menu}>
-                            <div>Menu</div>
-                            <div className={s.arrow}>
-                                <div className={s.arrow_down}></div>
+                        <div className={s.menu_container}>
+                            <div onClick={() => setDropMenu(!dropMenu)} className={dropMenu ? s.menu_vis : s.menu}>
+                                <div>Menu</div>
+                                <div className={dropMenu ? s.arrow_vis : s.arrow}>
+                                    <div className={dropMenu ? s.arrow_up : s.arrow_down}></div>
+                                </div>
+                            </div>
+                            <div className={dropMenu ? s.menu_drop_vis : s.menu_drop}>
+                                <NavLink onClick={() => setDropMenu(false)} to='/'>
+                                    <div className={s.drop_item}>
+                                        main page
+                                    </div>
+                                </NavLink>
+                                <NavLink onClick={() => setDropMenu(false)} to='/userrecognition'>
+                                    <div className={s.drop_item}>
+                                        recognize service
+                                    </div>
+                                </NavLink>
+
                             </div>
                         </div>
+
+                        <div className={s.userHi}>
+                            <div>
+                                <img className={s.user_logo} src={userLogo} alt="" />
+                            </div>
+                            <div> Hello, {user}!</div>
+                            <div>
+                                <img src={setting} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            case '/userrecognition': {
+                return (
+                    <div className={s.header_container}>
+                        <div className={s.menu_container}>
+                            <div onClick={() => setDropMenu(!dropMenu)} className={dropMenu ? s.menu_vis : s.menu}>
+                                <div>Menu</div>
+                                <div className={dropMenu ? s.arrow_vis : s.arrow}>
+                                    <div className={dropMenu ? s.arrow_up : s.arrow_down}></div>
+                                </div>
+                            </div>
+                            <div className={dropMenu ? s.menu_drop_vis : s.menu_drop}>
+                                <NavLink onClick={() => setDropMenu(false)} to='/'>
+                                    <div className={s.drop_item}>
+                                        main page
+                                    </div>
+                                </NavLink>
+                                <NavLink onClick={() => setDropMenu(false)} to='/tablespage'>
+                                    <div className={s.drop_item}>
+                                        collection
+                                    </div>
+                                </NavLink>
+
+                            </div>
+                        </div>
+
                         <div className={s.userHi}>
                             <div>
                                 <img className={s.user_logo} src={userLogo} alt="" />
