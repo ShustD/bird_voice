801c25b4f5a82ab1abe9accbfc144d70d14d3911
Dima Ivanov <latrey.9451@gmail.com>
2023-02-08 11:35:34 +0300

    adaptive

--- src/Components/TablePage/Tables/BoxInfo/BoxInfo.jsx
+++ src/Components/TablePage/Tables/BoxInfo/BoxInfo.jsx
@@ -1,28 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import s from './BoxInfo.module.scss'
 
 
 
 export const BoxInfo = (props) => {
+    const [isActive, setIsActive] = useState(false);
     const arr = Object.entries(props)
     const got = arr.map((e, index) => (
         <Info key={index} one={e[0]} two={e[1]} />
     ))
     return (
         <div className={s.box}>
-            <div className={s.title}>Bird</div>
-            {got}
+            <div className={s.title} onClick={() => setIsActive(false)}>Bird</div>
+            {isActive && <span className={s.arrow__up}></span>}
+           {isActive ? got : 
+                <div className={s.box__id} onClick={() => setIsActive(true)}>
+                    <div>ID</div>
+                    <div>{props.id}</div>
+                    <span className={s.arrow__down}></span>
+                </div>
+            }
         </div>
     )
 }
 
-const Info = ({one, two}) => {
-    const [isActive, setIsActive] = useState(false);
+const Info = ({ one, two }) => {
+
     return (
-        <div>
-            <div onClick={() => setIsActive(!isActive)} className={s.name__content}>{one} 
-            <span className={isActive ? s.arrow__up : s.arrow__down}></span></div>
-            {isActive && <div className={s.content}>{two}</div>}
-        </div>
+        <>
+            <div className={s.name__content}>{one}</div>
+            <div className={s.content}>{two}</div>
+        </>
     )
 }
